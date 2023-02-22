a79700fb723f91608bcf0fb8d7a2a0d9ab96c1df
Giuseppe Bonifati <110894389+Giuseppe-Bonifati@users.noreply.github.com>
2023-02-22 14:24:04 +0100

    Update main.py

--- main.py
+++ main.py
@@ -58,46 +58,60 @@ def start():
     short_break_sec = SHORT_BREAK_MIN * 60
     # get the second of the long break 20
     long_break_sec = LONG_BREAK_MIN * 60
-
+    
+    # 8 becuase after 4 times of short breaks of 5 min and 4  times 55 min of work, start the long break of 20 min 
     if reps % 8 == 0:
         play()
-        #in case user hide the widow when will be the break the  window will show up again 
+        #in case user hide the window, when will be the break the window will show up again 
         window.after(0,window.deiconify())
+        # call the function count_down with the long break 
         count_down(long_break_sec)
+        # change the label from Work to Break
         label .config(text = "Break", fg= RED)
+    # every 2 reps 1 of 55 min work then 5 min break , so every 2 reps 1 break     
     elif reps % 2 == 0:
         play()
-        #in case user hide the widow when will be the break the  window will show up again 
+        # in case user hide the widow when will be the break the  window will show up again 
         window.after(0,window.deiconify())
+        # call the function count_down with the short break
         count_down(short_break_sec)
+        # change the label from Work to Break
         label .config(text = "Break", fg = PINK)
     else:
-        count_down(sec)   
+        # else count down from the working time
+        count_down(sec)
+        # set label to Work
         label .config(text = "Work", fg= GREEN)
          
 
 
-
+# function to count down the time
 def count_down(count):
     '''Shows the time on work  ( count down ) and each time that time get to zero will call the function start
     that shows the breaks time , the function count_down will add a marks each time that will have a break'''
-    #ex 5*60 = 300 seconds so to get the minutes we need to divide 300/60 but to get the right minutes when the second are for 240 then we need to round to the min bigger number 
+    
+    #ex 5*60 = 300 seconds so to get the minutes we need to divide 300/60 but to get the right minutes when the second are for 238 then we need to round to the min bigger number that is 3 
     minute = math.floor(count / 60)
-    #count how many seconds is left
+    #count how many seconds is left using the rest % ,  if 240 sec then will be 240 % 60  = 0 if 241 % 60 then we have 4 min and 1 second 
     second = math.floor(count % 60)
     
     #to show the minutes and the seconds in a correct format
     if second < 10:
+        # format the int to a string
         second = f"0{second}"
     if minute < 10:
+        # inside the canvas time form tkinter we add the min and second formated as a string
         canvas.itemconfig(time, text=f"0{minute}:{second}")
     else:
+        # inside the canvas time form tkinter we add the min and second formated as a string
         canvas.itemconfig(time, text=f"{minute}:{second}")
     if count > 0:
+        # if count is > 0 the timer will change every 1 second and every time will call the function count_down() , the values that get the function will be -1 , in this way we count down the time
         global timer
         timer = window.after(1000, count_down, count -1)
 
     else:
+        # if count is 0 then we call the function start , to start again and we add a ✔ to the label check_marks
         start()
         session = math.floor(reps/ 2)
         marks = "".join("✔" for _ in range(session))
@@ -132,7 +146,7 @@ button_start.grid(row = 2, column = 0)
 button_reset = tkinter.Button(text = "Reset", highlightthickness=0, command=reset)
 button_reset.grid(row = 2, column = 2)
 
-
+# create the label to add ✔
 check_marks = tkinter.Label( fg = GREEN, bg = YELLOW, highlightthickness=0, font= ("bold"))
 check_marks.grid(row=3, column = 1)
 
